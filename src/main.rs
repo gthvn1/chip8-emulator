e3004d78b4378419512cc8e9d05eee8bdd742e3f
Guillaume <guillaume.thouvenin@vates.tech>
2024-04-30 13:51:10 +0200

    change the size of pixel to 20x20

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,10 @@ fn main() {
     let filename = &a[1];
     log::info!("Emulating {filename}");
 
-    let pixel_width = 10_i32;
-    let pixel_height = 10_i32;
+    let pixel_width = 20_i32;
+    let pixel_height = 20_i32;
 
+    // Use a window of 1280 x 640
     let screen_width: i32 = RESOLUTION.0 * pixel_width;
     let screen_height: i32 = RESOLUTION.1 * pixel_height;
 
@@ -52,33 +53,69 @@ fn main() {
 
         for (i, byte) in fb.iter().enumerate() {
             let v = i as i32;
-            let x: i32 = ((v * 8) % 64) * 10;
-            let y: i32 = (v / 8) * 10;
+            let x: i32 = ((v * 8) % 64) * pixel_width;
+            let y: i32 = (v / 8) * pixel_height;
 
             // We draw a 10x10 rectangle for each bit set to 1
             if byte & 0x80 == 0x80 {
                 draw_rectangle(x, y, pixel_width, pixel_height, color::GREEN);
             }
             if byte & 0x40 == 0x40 {
-                draw_rectangle(x + 10, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(x + pixel_width, y, pixel_width, pixel_height, color::GREEN);
             }
             if byte & 0x20 == 0x20 {
-                draw_rectangle(x + 20, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(
+                    x + 2 * pixel_width,
+                    y,
+                    pixel_width,
+                    pixel_height,
+                    color::GREEN,
+                );
             }
             if byte & 0x10 == 0x10 {
-                draw_rectangle(x + 30, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(
+                    x + 3 * pixel_width,
+                    y,
+                    pixel_width,
+                    pixel_height,
+                    color::GREEN,
+                );
             }
             if byte & 0x8 == 0x8 {
-                draw_rectangle(x + 40, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(
+                    x + 4 * pixel_width,
+                    y,
+                    pixel_width,
+                    pixel_height,
+                    color::GREEN,
+                );
             }
             if byte & 0x4 == 0x4 {
-                draw_rectangle(x + 50, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(
+                    x + 5 * pixel_width,
+                    y,
+                    pixel_width,
+                    pixel_height,
+                    color::GREEN,
+                );
             }
             if byte & 0x2 == 0x2 {
-                draw_rectangle(x + 60, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(
+                    x + 6 * pixel_width,
+                    y,
+                    pixel_width,
+                    pixel_height,
+                    color::GREEN,
+                );
             }
             if byte & 0x1 == 0x1 {
-                draw_rectangle(x + 70, y, pixel_width, pixel_height, color::GREEN);
+                draw_rectangle(
+                    x + 7 * pixel_width,
+                    y,
+                    pixel_width,
+                    pixel_height,
+                    color::GREEN,
+                );
             }
         }
 
